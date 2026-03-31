9f7f5e46c387f29c9179dd1f7c21c52f25c0dd6c
Jenish094 <prakashjenish3@gmail.com>
2026-04-01 09:49:12 +1030

    Refactor imports and adjust formatting in multiple files

--- src/hooks/useProviderScrape.tsx
+++ src/hooks/useProviderScrape.tsx
@@ -1,4 +1,8 @@
-import { FullScraperEvents, RunOutput, ScrapeMedia } from "@jenish094/providers";
+import {
+  FullScraperEvents,
+  RunOutput,
+  ScrapeMedia,
+} from "@jenish094/providers";
 import { RefObject, useCallback, useEffect, useRef, useState } from "react";
 
 import { isExtensionActiveCached } from "@/backend/extension/messaging";
