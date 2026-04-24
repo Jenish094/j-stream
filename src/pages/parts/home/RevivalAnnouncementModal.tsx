703b99001d3c7c3bd1bfd749b87d6ac41a8bd7e7
Jenish Pathak <46525717+Jenish094@users.noreply.github.com>
2026-04-24 13:13:38 +0930

    Reformat announcement text in RevivalAnnouncementModal
    Reformat text in RevivalAnnouncementModal for clarity.

--- src/pages/parts/home/RevivalAnnouncementModal.tsx
+++ src/pages/parts/home/RevivalAnnouncementModal.tsx
@@ -51,9 +51,9 @@ export function RevivalAnnouncementModal() {
                 </div>
                 <div className="space-y-4 text-base text-type-secondary">
                   <p className="text-white font-bold border-l-2 border-white pl-3">
-                    This is a fork of the original P-Stream. I (Jenish094)
-                    hold no ownership over the original project and 
-                    make no claim to it.
+                    This is a fork of the original P-Stream. I (Jenish094) hold
+                    no ownership over the original project and make no claim to 
+                    it.
                   </p>
                   <p>
                     The original main contributor has stepped down and will no
